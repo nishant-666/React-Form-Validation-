b7dbd1df2d9e8fd06e95fdb8f309e92c01965e2b
Nishant Kumar <nishant.k@doodleblue.com>
2021-10-09 11:20:48 +0530

    React Hook Form

---
+++ src/components/Form.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Form, Button } from 'semantic-ui-react';
+import { useForm } from "react-hook-form";
+
+export default function FormValidation() {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+    return (
+        <div>
+            <Form onSubmit={handleSubmit(onSubmit)}>
+                <Form.Field>
+                    <label>First Name</label>
+                    <input
+                        placeholder='First Name'
+                        type="text"
+                        {...register("firstName", { required: true, maxLength: 10 })}
+                    />
+                </Form.Field>
+                {errors.firstName && <p className="text-error">Please check the First Name</p>}
+                <Form.Field>
+                    <label>Last Name</label>
+                    <input
+                        placeholder='Last Name'
+                        type="text"
+                        {...register("lastName", { required: true, maxLength: 10 })}
+                    />
+                </Form.Field>
+                {errors.lastName && <p className="text-error">Please check the Last Name</p>}
+                <Form.Field>
+                    <label>Email</label>
+                    <input
+                        placeholder='Email'
+                        type="email"
+                        {...register("email",
+                            {
+                                required: true,
+                                pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                            })}
+                    />
+                </Form.Field>
+                {errors.email && <p className="text-error">Please check the Email</p>}
+                <Form.Field>
+                    <label>Password</label>
+                    <input
+                        placeholder='Password'
+                        type="password"
+                        {...register("password", {
+                            required: true,
+                            pattern: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,15}$/
+                        })}
+                    />
+                </Form.Field>
+                {errors.password && <p className="text-error">Please check the Password</p>}
+                <Button type='submit'>Submit</Button>
+            </Form>
+        </div>
+    )
+}
